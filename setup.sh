53a13e740db019015092aabb056215e76f6c188d
Austin <asnair@users.noreply.github.com>
2017-10-20 14:37:04 -0400

    Files for blog
    Vagrant DEV set up

--- setup.sh
+++ setup.sh
@@ -1,42 +1,59 @@
+#Set up script
 
-set -e
-# set environment variables
-cat >> ~/.profile << EOF
-export LANG=C
-export LC_CTYPE=C
-export POSTGIS_GDAL_ENABLED_DRIVERS=GTiff
-export POSTGIS_ENABLE_OUTDB_RASTERS=1
-EOF
-sudo bash -c "cat > /etc/apt/sources.list.d/pgdg.list" << EOF
-deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main
-EOF
-wget --quiet -O ACCC4CF8.asc https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-# update apt-get repository
-sudo apt-get update
-# install add-apt-repository tool
-sudo apt-get install -y --no-install-recommends python-software-properties
-# add ubuntu-toolchain-r/test PPA
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test
-# update apt-get repository
-sudo apt-get update
-# install base dependencies
-sudo apt-get install -y --no-install-recommends \
-    g++-6 pkg-config libcurl4-openssl-dev python-dev curl git redis-server \
-    libpq-dev postgresql-9.5-postgis-2.2 postgresql-9.5-postgis-2.2-scripts postgresql-contrib-9.5 \
-    libfreetype6-dev libpng-dev libffi-dev python-pip python-mapscript cgi-mapserver
-# set GCC 6 as default
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-6 60 --slave /usr/bin/g++ g++ /usr/bin/g++-6
-# install skylines and the python dependencies
-cd /skylines
-sudo -H pip install -r requirements.txt --no-binary greenlet #add -U Celery, xcsoar
-# create PostGIS databases
-sudo sudo -u postgres createuser -s vagrant
-sudo sudo -u postgres createdb skylines -O vagrant
-sudo sudo -u postgres psql -d skylines -c 'CREATE EXTENSION postgis;'
-sudo sudo -u postgres psql -d skylines -c 'CREATE EXTENSION fuzzystrmatch;'
-sudo sudo -u postgres createdb skylines_test -O vagrant
-sudo sudo -u postgres psql -d skylines_test -c 'CREATE EXTENSION postgis;'
-sudo sudo -u postgres psql -d skylines_test -c 'CREATE EXTENSION fuzzystrmatch;'
-sudo sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'secret123';"
-./manage.py db create
-./manage.py import welt2000 --commit
+DBHOST=localhost
+DBNAME=dbname
+DBUSER=dbuser
+DBPASSWD=test12345
+
+
+sudo apt-get update -y
+
+sudo apt-get install git-core -y
+
+sudo apt-get install dpkg-dev -y
+
+git config --global color.ui true
+
+echo '.*swp' > ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
+
+sudo apt-get install tmux -y
+sudo apt-get install vim -y
+
+sudo apt-get install build-essential -y
+
+sudo apt-get install apache2 -y
+
+# MySQL setup for development purposes ONLY
+echo -e "\n--- Install MySQL specific packages and settings ---\n"
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASSWD"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASSWD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $DBPASSWD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASSWD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASSWD"
+debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect none"
+apt-get -y install mysql-server >> /vagrant/vm_build.log 2>&1
+
+echo -e "\n--- Setting up our MySQL user and db ---\n"
+mysql -uroot -p $DBPASSWD -e "CREATE DATABASE $DBNAME" >> /vagrant/vm_build.log 2>&1
+mysql -uroot -p $DBPASSWD -e "grant all privileges on $DBNAME.* to '$DBUSER'@'localhost' identified by '$DBPASSWD'" > /vagrant/vm_build.log 2>&1
+
+
+sudo apt-get install python-dev python-virtualenv python-pip -y
+
+echo -e "\n--- Enabling mod-rewrite ---\n"
+a2enmod rewrite >> /vagrant/vm_build.log 2>&1
+
+echo -e "\n--- Allowing Apache override to all ---\n"
+sed -i "s/AllowOverride None/AllowOverride All/g" /etc/apache2/apache2.conf
+
+echo -e "\n--- Setting document root to public directory ---\n"
+rm -rf /var/www/html
+ln -fs /vagrant/public /var/www/html
+
+echo -e "\n--- Restarting Apache ---\n"
+#service apache2 restart >> /vagrant/vm_build.log 2>&
+
+echo -e "Use the following commands to save changes and commit the changes: \n git commit \n git status \n git push origin master"
+echo -e "\n FinishedH"
